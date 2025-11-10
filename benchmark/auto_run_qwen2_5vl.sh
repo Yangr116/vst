d19d7bdb7f1e71e71054fb0d5d15a43f4d44f9dd
Yangr116 <rayyang0116@gmail.com>
2025-11-10 09:54:15 +0800

    update

--- benchmark/auto_run_qwen2_5vl.sh
+++ benchmark/auto_run_qwen2_5vl.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 set -x
 
-export SEED_MODELS_LOGGING_LEVEL=WARN
-export OMNISTORE_LOGGING_LEVEL=ERROR
-export BPEX_NO_WARN_ON_UNTUNED_CASE=1
-export TOKENIZERS_PARALLELISM=false
-export VESCALE_SINGLE_DEVICE_RAND=0
-export TF_CPP_MIN_LOG_LEVEL=2
-export CUDA_LAUNCH_BLOCKING=1
-
-NNODES=${NNODES:=$ARNOLD_WORKER_NUM}
-NPROC_PER_NODE=${NPROC_PER_NODE:=$ARNOLD_WORKER_GPU}
-NPROC_PER_NODE=${NPROC_PER_NODE:=$ARNOLD_WORKER_GPU_PER_NODE}
-NODE_RANK=${NODE_RANK:=$ARNOLD_ID}
-MASTER_ADDR=${MASTER_ADDR:=$ARNOLD_WORKER_0_HOST}
-MASTER_ADDR=${MASTER_ADDR:=$ARNOLD_EXECUTOR_0_HOST}
-MASTER_PORT=${MASTER_PORT:=$(echo "$ARNOLD_WORKER_0_PORT" | cut -d "," -f 1)}
-MASTER_PORT=${MASTER_PORT:=$(echo "$ARNOLD_EXECUTOR_0_PORT" | cut -d "," -f 1)}
+NNODES=1
+NPROC_PER_NODE=8
+NODE_RANK=0
+MASTER_PORT=8888
 
 model_path=$1
 datetime=$(date +%Y-%m-%d_%H-%M-%S)
@@ -39,7 +27,7 @@ work_dir="work_dirs/$work_dir"
 mkdir -p $work_dir
 
 torchrun --nnodes=$NNODES --nproc-per-node $NPROC_PER_NODE --node-rank $NODE_RANK \
-  --master-addr=$MASTER_ADDR --master-port=$MASTER_PORT run.py --config config_qwen2_5.json --model_path "$model_path" --work-dir "$work_dir" 2>&1 | tee "work_dirs/$modified_string-log.txt"
+  --master-port=$MASTER_PORT run.py --config config_qwen2_5.json --model_path "$model_path" --work-dir "$work_dir" 2>&1 | tee "work_dirs/$modified_string-log.txt"
 
 # vsibench
 bash eval_vsibench.sh $model_path $work_dir
